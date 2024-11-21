2cfee0056620671668649df2ceb1f711069854d9
Mohamed Yassin <arman.mohamedyassin@stedin.net>
2024-11-21 12:07:04 +0100

    initial prj setup during lunch break

---
+++ ElecMarketSim/ElecMarketSim1.cpp
@@ -0,0 +1,12 @@
+﻿// ElecMarketSim1.cpp : Defines the entry point for the application.
+//
+
+#include "ElecMarketSim1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
